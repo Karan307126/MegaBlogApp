097720a37cd605ac921c247a3f597e5bf827845f
Karan Sarvaiya <sarvaiyakaran2003@gmail.com>
2024-07-20 18:12:52 +0530

    Add tiny editor and post components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,29 @@ import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./header/LogoutBtn";
+import Select from "./Select";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+import Protected from "./AuthLayout";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Signup from "./Signup";
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Select,
+  RTE,
+  PostForm,
+  Protected,
+  Button,
+  Input,
+  Login,
+  PostCard,
+  Signup,
+};
